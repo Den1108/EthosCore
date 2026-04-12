c3addb7178cd2d5a841f80f73de06b6b79558744
Den1108 <zeromen945@gmail.com>
2026-04-12 22:00:48 +0300

    исправление ошибки

---
+++ src/main/java/studio/arcana/ethos/client/DialogueOption.java
@@ -0,0 +1,11 @@
+package studio.arcana.ethos.client;
+
+public class DialogueOption {
+    public final String text;
+    public final Runnable action;
+
+    public DialogueOption(String text, Runnable action) {
+        this.text = text;
+        this.action = action;
+    }
+}
